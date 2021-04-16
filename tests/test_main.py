284e0506935242b9b0ad7374fc1759135692a65b
justusbersten <justusbersten@gmail.com>
2021-04-16 22:55:33 +0200

    add: test  failure

--- tests/test_main.py
+++ tests/test_main.py
@@ -30,8 +30,19 @@ def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -
     runner.invoke(__main__.main)
     assert mock_requests_get.called
 
+
 def test_main_uses_count_and_gender(runner: CliRunner, mock_requests_get: Mock) -> None:
     """It uses the English Wikipedia by default."""
     runner.invoke(__main__.main)
     args, _ = mock_requests_get.call_args
     assert ("10" in args[0] and "a" in args[0])
+
+
+def test_main_fails_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
+    """It exists with a non-zero status code if the request fails."""
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(__main__.main)
+    assert result.exit_code == 1
+
