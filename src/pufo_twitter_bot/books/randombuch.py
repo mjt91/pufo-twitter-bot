b004704013f089016e3c7ca6527b7b3827d86453
justusbersten <justusbersten@gmail.com>
2021-04-17 12:13:30 +0200

    refactor: improve requests block and exceptions

--- src/pufo_twitter_bot/books/randombuch.py
+++ src/pufo_twitter_bot/books/randombuch.py
@@ -1,23 +1,36 @@
 """Random book titles generator."""
+from typing import List
 
-from bs4 import BeautifulSoup
+import click
 import requests
+from bs4 import BeautifulSoup
 
 
-BOOK_URL = "https://www.buchtitelgenerator.de/aasdasdas"
+BOOK_URL = "https://www.buchtitelgenerator.de/"
 
-def get_booklist_from_html():
-    r = requests.get(BOOK_URL)
-    content = r.content
 
-    soup = BeautifulSoup(content, 'html.parser')
-    books = soup.find_all("div", class_="panel-heading")
+def get_booklist_from_html() -> List[str]:
+    """Retrieve a list of 5 random books from URL
 
-    if books:
-        return [book.text.strip() for book in books]
-    else:
-        raise TypeError(f"Error parsing the '{BOOK_URL}' html source")
+    Raises:
+        TypeError: If it fails to retrieve the books.
 
+    Returns:
+        List[str]: list with 5 strings with book titles
+    """
+    try:
+        with requests.get(BOOK_URL) as response:
+            response.raise_for_status()
+            content = response.content
+
+            soup = BeautifulSoup(content, "html.parser")
+            books = soup.find_all("div", class_="panel-heading")
+
+            return [book.text.strip() for book in books]
+
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
 
 
 if __name__ == "__main__":
