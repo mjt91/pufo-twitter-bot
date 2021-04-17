84383d52081d2ee6e9e126df53277c6fc78bc0b8
justusbersten <justusbersten@gmail.com>
2021-04-17 11:58:52 +0200

    add: randombuch module setup initial func

--- src/pufo_twitter_bot/books/randombuch.py
+++ src/pufo_twitter_bot/books/randombuch.py
@@ -1,15 +1,25 @@
 """Random book titles generator."""
+
 from bs4 import BeautifulSoup
 import requests
 
 
-BOOK_URL = "https://www.buchtitelgenerator.de/"
+BOOK_URL = "https://www.buchtitelgenerator.de/aasdasdas"
 
-def get_book_html():
+def get_booklist_from_html():
     r = requests.get(BOOK_URL)
-
     content = r.content
 
-    soup = BeautifulSoup(content)
+    soup = BeautifulSoup(content, 'html.parser')
+    books = soup.find_all("div", class_="panel-heading")
 
-    print(soup.prettify)
+    if books:
+        return [book.text.strip() for book in books]
+    else:
+        raise TypeError(f"Error parsing the '{BOOK_URL}' html source")
+
+
+
+if __name__ == "__main__":
+    books = get_booklist_from_html()
+    print(books)
