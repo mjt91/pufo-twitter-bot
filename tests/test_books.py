e2d619988cc4d781d7d1458c333279fb8bf4d580
justusbersten <justusbersten@gmail.com>
2021-04-20 07:04:27 +0200

    add: test book return types

--- tests/test_books.py
+++ tests/test_books.py
@@ -1,1 +1,15 @@
 """Test cases for the books module."""
+from typing import List
+
+from pufo_twitter_bot.books import randombuch
+
+
+def test_buchtitelgenerator_returns_list() -> None:
+    books = randombuch.buchtitelgenerator()
+    assert isinstance(books, list)
+
+
+def test_buchtitelgenerator_returns_list_of_strs() -> None:
+    books = randombuch.buchtitelgenerator()
+    for book in books:
+        assert isinstance(book, str)
