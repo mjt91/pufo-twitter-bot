74a11da8dd415d5882ecdfd7aa41399d6daa8b87
justusbersten <justusbersten@gmail.com>
2021-04-29 07:10:12 +0200

    add: docstrings

--- tests/test_books.py
+++ tests/test_books.py
@@ -5,11 +5,13 @@ from pufo_twitter_bot.books import randombuch
 
 
 def test_buchtitelgenerator_returns_list() -> None:
+    """It returns a list."""
     books = randombuch.buchtitelgenerator()
     assert isinstance(books, list)
 
 
 def test_buchtitelgenerator_returns_list_of_strs() -> None:
+    """Each book is of type str."""
     books = randombuch.buchtitelgenerator()
     for book in books:
         assert isinstance(book, str)
