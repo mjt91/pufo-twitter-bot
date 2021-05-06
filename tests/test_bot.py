68a4918d54c03e0d1e58f83b54570f6ec9e2b626
justusbersten <justusbersten@gmail.com>
2021-05-06 19:40:55 +0200

    refactor: remove imports

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,12 +1,4 @@
 """Test cases for the bot module."""
-from pytest_mock import MockFixture
-from pytest_mock import mocker
-import os
-from pufo_twitter_bot.bot import twitter
-
-import tweepy
-
-import pytest
 
 
 def test_twitter_api(mock_tweepy_api):
