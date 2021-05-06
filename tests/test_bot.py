b0cfae3cd21159aaef6c87d2cdfac1f34bcd914e
justusbersten <justusbersten@gmail.com>
2021-05-06 19:45:43 +0200

    add: types to test

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,6 +1,7 @@
 """Test cases for the bot module."""
+from unittest.mock import Mock
 
 
-def test_twitter_api(mock_tweepy_api):
+def test_twitter_api(mock_tweepy_api: Mock) -> None:
     # tw = mocker.patch.object(tweepy, "API", autospec=True)
     mock_tweepy_api.update_status("Hello Tweepy")
