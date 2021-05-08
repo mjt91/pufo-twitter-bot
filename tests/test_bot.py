81f43eaee536aa62947f982d7243139d0ef90ff1
justusbersten <justusbersten@gmail.com>
2021-05-08 22:51:47 +0200

    add: doc string in public function

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -3,5 +3,6 @@ from unittest.mock import Mock
 
 
 def test_twitter_api(mock_tweepy_api: Mock) -> None:
+    """It succeeds to update the status."""
     # tw = mocker.patch.object(tweepy, "API", autospec=True)
     mock_tweepy_api.update_status("Hello Tweepy")
