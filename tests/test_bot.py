6254e4ff30f154d6037a70c26e5b0a835e704537
justusbersten <justusbersten@gmail.com>
2021-10-16 00:10:32 +0200

    tests: refactor the tests for bot

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -6,6 +6,7 @@ import click
 import pytest
 
 from pufo_twitter_bot.bot import twitter
+from pufo_twitter_bot.bot.twitter import TwitterBot
 
 
 def test_twitter_api(mock_tweepy_api: Mock) -> None:
@@ -13,30 +14,29 @@ def test_twitter_api(mock_tweepy_api: Mock) -> None:
     mock_tweepy_api.update_status("Hello Tweepy")
 
 
-def test_retrieve_keys(mock_environ_variables: Mock) -> None:
+def test_retrieve_keys(mock_environ_variables: Mock, mock_tweepy_api: Mock) -> None:
     """It succeeds to retrives the keys."""
-    ck, cs, at, ats = twitter.retrieve_keys()
-    assert ck == "consumer_test_key"
-    assert cs == "consumer_test_secret_Key"
-    assert at == "access_test_token"
-    assert ats == "access_test_token_secret"
+    twb = TwitterBot(tweet="_")
+    assert twb.consumer_key == "consumer_test_key"
+    assert twb.consumer_secret == "consumer_test_secret_Key"
+    assert twb.access_token == "access_test_token"
+    assert twb.access_token_secret ==  "access_test_token_secret"
 
 
-def test_retrieve_keys_with_none(mock_environ_variables: Mock) -> None:
+def test_retrieve_keys_with_none(mock_environ_variables: Mock, mock_tweepy_api: Mock) -> None:
     """It returns the variables even if one is of None type."""
     # Remove mocked consumer key from environ
     del os.environ["CONSUMER_KEY"]
-    ck, _, _, _ = twitter.retrieve_keys()
-    assert ck is None
+    twb = TwitterBot(tweet="_")
+    assert twb.consumer_key is None
 
 
 def test_create_api_fails(mock_environ_variables: Mock) -> None:
     """It raises an error when one environment variable is missing."""
     del os.environ["CONSUMER_KEY"]
-    click.ClickException
     # with pytest.raises(tweepy.error.TweepError):
     with pytest.raises(click.ClickException):
-        twitter.create_api()
+        twb = TwitterBot(tweet="_")
 
 
 def test_validate_tweet_succeeds() -> None:
@@ -49,6 +49,5 @@ def test_validate_tweet_fails() -> None:
     """The length validation for the tweet fails."""
     with open("./tests/data/tweet-test.txt", "r") as file:
         test_tweet = file.read()
-
     with pytest.raises(ValueError):
         twitter.validate_tweet(test_tweet)
