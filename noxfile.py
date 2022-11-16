45598fd54b6bb90e797aa02e6a735058bf85cc95
justusbersten <justusbersten@gmail.com>
2022-11-16 22:48:23 +0100

    update coverage session

--- noxfile.py
+++ noxfile.py
@@ -125,17 +125,13 @@ def tests(session: Session) -> None:
             session.notify("coverage")
 
 
-@session
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
-    # Do not use session.posargs unless this is the only session.
-    nsessions = len(session._runner.manifest)
-    has_args = session.posargs and nsessions == 1
-    args = session.posargs if has_args else ["report"]
+    args = session.posargs or ["report"]
 
     session.install("coverage[toml]")
 
-    if not has_args and any(Path().glob(".coverage.*")):
+    if not session.posargs and any(Path().glob(".coverage.*")):
         session.run("coverage", "combine")
 
     session.run("coverage", *args)
