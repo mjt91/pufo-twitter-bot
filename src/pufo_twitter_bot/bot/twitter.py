729e73d874233044d7bd15b7c9558e596f839f01
justusbersten <justusbersten@gmail.com>
2023-08-24 11:39:19 +0200

    add client

--- src/pufo_twitter_bot/bot/twitter.py
+++ src/pufo_twitter_bot/bot/twitter.py
@@ -62,7 +62,12 @@ class TwitterBot:
         """
         # Get all API keys from ENV variables
         self._retrieve_keys()
-
+        client = tweepy.Client(
+            consumer_key=self.consumer_key,
+            consumer_secret=self.consumer_secret,
+            access_token=self.access_token,
+            access_token_secret=self.access_token_secret
+        )
         auth = tweepy.OAuth2AppHandler(self.consumer_key, self.consumer_secret)
         # auth.set_access_token(self.access_token, self.access_token_secret)
         api = tweepy.API(auth)
