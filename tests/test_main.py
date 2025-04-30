7a3a68bf71bc6e67603a67ecedab4a15668b48d2
justusbersten <justusbersten@gmail.com>
2025-04-30 15:50:02 +0200

    Remove skipped tests and update test for printing authors in test_main.py. Adjusted test parameters for improved functionality.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,9 @@
 """Test cases for the __main__ module."""
 
 import random
-from unittest.mock import Mock
 from unittest.mock import patch
 
 import pytest
-import requests  # type: ignore
 from click.testing import CliRunner
 
 from pufo_twitter_bot import __main__
@@ -17,75 +15,10 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
-    assert result.exit_code == 0
-
-
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
-def test_main_succeeds_offenedaten(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main, ["-s", "offenedaten"])
-    assert result.exit_code == 0
-
-
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
-def test_main_succeeds_with_tweet(
-    runner: CliRunner, mock_tweepy_api: Mock, mock_environ_variables: Mock
-) -> None:
-    """It exits with a status of code zero with tweet option."""
-    result = runner.invoke(__main__.main, ["-c", "2", "--tweet"])
-    assert result.exit_code == 0
-
-
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
 @patch("pufo_twitter_bot.__main__.FIRST_NAMES", "tests/data/first-names-test.json")
 @patch("pufo_twitter_bot.__main__.LAST_NAMES", "tests/data/last-names-test.txt")
-def test_main_prints_fallback_authors(runner: CliRunner) -> None:
+def test_main_prints_authors(runner: CliRunner) -> None:
     """It prints the names of the fallback authors."""
     random.seed(2)  # use seed to guarantee testability
-    result = runner.invoke(__main__.main, ["--count", "2", "--source", "offenedaten"])
+    result = runner.invoke(__main__.main, ["--count", "2"])
     assert "Peter Lorem" in result.output and "Lisa Ipsum" in result.output
-
-
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
-def test_main_prints_authors(runner: CliRunner, mock_requests_get: Mock) -> None:
-    """It prints the names of the authors."""
-    result = runner.invoke(__main__.main)
-    print(result)
-    assert "Peter Lorem" in result.output and "Lisa Ipsum" in result.output
-
-
-def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -> None:
-    """It invokdes requests get."""
-    runner.invoke(__main__.main)
-    assert mock_requests_get.called
-
-
-@pytest.mark.skip(reason="FIX mocker for requests get!!")
-def test_main_uses_count_and_gender(runner: CliRunner, mock_requests_get: Mock) -> None:
-    """It uses the English Wikipedia by default."""
-    runner.invoke(__main__.main)
-    args, _ = mock_requests_get.call_args
-    print(args)
-    assert "10" in args[0] and "a" in args[0]
-
-
-def test_main_fails_on_request_error(
-    runner: CliRunner, mock_requests_get: Mock
-) -> None:
-    """It exists with a non-zero status code if the request fails."""
-    mock_requests_get.side_effect = Exception("Boom")
-    result = runner.invoke(__main__.main)
-    assert result.exit_code == 1
-
-
-def test_main_prints_message_on_request_error(
-    runner: CliRunner, mock_requests_get: Mock
-) -> None:
-    """It prints an error message if the request fails."""
-    mock_requests_get.side_effect = requests.RequestException
-    result = runner.invoke(__main__.main)
-    assert "Error" in result.output
